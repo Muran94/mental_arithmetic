b19b2d954ec60dd3ecdcf29d8c57df67a5655bdb
Muran94 <headiv94@gmail.com>
2017-10-26 19:31:38 +0900

    引き算も追加

---
+++ arithmetic/arithmetic.py
@@ -0,0 +1,99 @@
+""" メモ
+四則演算のそれぞれのクラスのinitとstartとget_user_answerは共通化できる。要リファクタリング。
+""""
+
+from random import randint
+import re
+
+class Addition:
+    def __init__(self):
+        self.correct_answer_count = 0
+
+    def start(self):
+        for i in range(1, 11):
+            problem = self.create_problem()
+            self.get_user_answer(problem)
+        print("結果 : {0}/10".format(self.correct_answer_count))
+
+    def create_problem(self):
+        num1 = randint(1, 1000)
+        num2 = randint(1, 1000)
+        self.correct_answer = num1 + num2
+        return "{0:>3d} + {1:>3d} = ".format(num1, num2)
+
+    def get_user_answer(self, problem):
+        while True:
+            user_answer = input(problem)
+
+            if re.match(r"^[0-9]+$", user_answer):
+                if int(user_answer) == self.correct_answer:
+                    self.correct_answer_count += 1
+                    print("正解")
+                    break
+                else:
+                    print("不正解")
+                    break
+            else:
+                print("入力された値に誤りがあります")
+
+class Subtraction:
+    def __init__(self):
+        self.correct_answer_count = 0
+
+    def start(self):
+        for i in range(1, 11):
+            problem = self.create_problem()
+            self.get_user_answer(problem)
+        print("結果 : {0}/10".format(self.correct_answer_count))
+
+    def create_problem(self):
+        num1 = randint(1, 999)
+        num2 = randint(num1, 1000)
+        self.correct_answer = num2 - num1
+        return "{0:>3d} - {1:>3d} = ".format(num2, num1)
+
+    def get_user_answer(self, problem):
+        while True:
+            user_answer = input(problem)
+
+            if re.match(r"^[0-9]+$", user_answer):
+                if int(user_answer) == self.correct_answer:
+                    self.correct_answer_count += 1
+                    print("正解")
+                    break
+                else:
+                    print("不正解")
+                    break
+            else:
+                print("入力された値に誤りがあります")
+
+class Multiplication:
+    def __init__(self):
+        self.correct_answer_count = 0
+
+    def start(self):
+        for i in range(1, 11):
+            problem = self.create_problem()
+            self.get_user_answer(problem)
+        print("結果 : {0}/10".format(self.correct_answer_count))
+
+    def create_problem(self):
+        num1 = randint(1, 999)
+        num2 = randint(num1, 1000)
+        self.correct_answer = num2 - num1
+        return "{0:>3d} - {1:>3d} = ".format(num2, num1)
+
+    def get_user_answer(self, problem):
+        while True:
+            user_answer = input(problem)
+
+            if re.match(r"^[0-9]+$", user_answer):
+                if int(user_answer) == self.correct_answer:
+                    self.correct_answer_count += 1
+                    print("正解")
+                    break
+                else:
+                    print("不正解")
+                    break
+            else:
+                print("入力された値に誤りがあります")
--- mental_arithmetic.py
+++ mental_arithmetic.py
@@ -1,39 +1,6 @@
 from config.game_setting import GameSetting
-
+from arithmetic.arithmetic import Addition, Subtraction
 import re
-
-from random import randint
-
-class Addition:
-    def __init__(self):
-        self.correct_answer_count = 0
-
-    def start(self):
-        for i in range(1, 11):
-            problem = self.create_problem()
-            self.get_user_answer(problem)
-        print("結果 : {0}/10".format(self.correct_answer_count))
-
-    def create_problem(self):
-        num1 = randint(1, 1000)
-        num2 = randint(1, 1000)
-        self.correct_answer = num1 + num2
-        return "{0:>3d} + {1:>3d} = ".format(num1, num2)
-
-    def get_user_answer(self, problem):
-        while True:
-            user_answer = input(problem)
-
-            if re.match(r"^[0-9]+$", user_answer):
-                if int(user_answer) == self.correct_answer:
-                    self.correct_answer_count += 1
-                    print("正解")
-                    break
-                else:
-                    print("不正解")
-                    break
-            else:
-                print("入力された値に誤りがあります")
 
 class MentalArithmeticApplication:
     def __init__(self, selected_game):
@@ -45,7 +12,8 @@ class MentalArithmeticApplication:
             addition = Addition()
             addition.start()
         elif selected_game == "s":
-            pass
+            subtraction = Subtraction()
+            subtraction.start() # ここらへんってクラスメソッドとして定義するだけで十分じゃないの？
         elif selected_game == "m":
             pass
         elif selected_game == "d":
